9165f63cbd78e3ac748030a4c67e1cd0a311fb6e
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-10-26 17:53:29 +0600

    added tailwind, vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-red-600'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
