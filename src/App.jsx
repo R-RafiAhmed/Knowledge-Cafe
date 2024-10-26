9907667a6e3ac14ce3d774fe2c3b54b23002ecd0
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-10-26 20:19:17 +0600

    fetched demo data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './component/Header/Header'
+import Blogs from './component/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl text-red-600'>Vite + React</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
